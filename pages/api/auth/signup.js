bbfe11298d7c5cc9a188e340bca5ec80c8d8efb2
nenadmne <cosovicnenad14@gmail.com>
2023-12-20 21:07:11 +0100

    Updating signup api file

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,30 @@
+import { hashPassword } from "../../../lib/auth";
+import { connectToDatabase } from "../../../lib/db";
+
+export default async function signUpHandler(req, res) {
+  const data = req.body;
+  const { email, password } = data;
+
+  if (
+    !email ||
+    !password ||
+    !email.includes("@") ||
+    email.trim().length === 0 ||
+    password.trim().length === 0
+  ) {
+    return res.status(422).json({ message: "Invalid input data" });
+  }
+
+  const client = await connectToDatabase();
+
+  const db = client.db();
+
+  const hashedPassword = hashPassword(password);
+
+  const result = await db.collection("users").insertOne({
+    email: email,
+    password: hashedPassword,
+  });
+
+  res.status(200).json({ message: "Successfully added user", result: result });
+}
